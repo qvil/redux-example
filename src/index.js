56455d0fea5f9e22374a856afb4fb6226fbe3d62
tshyeon <qvil1127@gmail.com>
2016-10-14 16:54:14 +0900

    redux example - dispatch, unsubscribe

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ import App from './components/App';
 
 import { createStore } from 'redux';
 import reducers from './reducers';
+import * as actions from './actions';
 
 const store = createStore(reducers);
 
 console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(actions.increment());
+store.dispatch(actions.increment());
+store.dispatch(actions.increment());
+store.dispatch(actions.decrement());
+store.dispatch(actions.setColor([200, 200, 200]));
+
+unsubscribe();
+
+store.dispatch(actions.setColor([210, 210, 210]));
 
 ReactDOM.render(
     <App/>,
